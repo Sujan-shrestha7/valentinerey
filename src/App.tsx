6cb18425c494d463f9f272ac58895b2fb5786b47
Sujan-shrestha7 <sujanstha5732@gmail.com>
2026-02-14 11:53:11 +0545

    fix base path for netlify

--- src/App.tsx
+++ src/App.tsx
@@ -21,11 +21,11 @@ export default function Page() {
       "But :*(",
       "I am going to die",
       "Yep im dead",
-      "ok ur talking to nathan's ghost",
+      "ok ur talking with sujan's ghost",
       "please babe",
       "Jau mampakha nisthurii:(",
       "Pleaseeeee pleaseeee",
-      "No :(",
+      "No :( 😭😭😭😭",
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
